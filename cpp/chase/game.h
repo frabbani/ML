4c2cd5415aa9d47ad3174692498b89a6ed58723c
frabbani <f.rabbani@gmail.com>
2024-07-30 22:39:36 -0500

    chase is on! you better run!

---
+++ cpp/chase/game.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "vector.h"
+#include "neural.h"
+
+struct Game {
+  Vector2 runner;
+  Vector2 chaser;
+
+  std::vector<Vector2> trainingPts;
+  std::vector<Vector2> trainingPts2;
+
+  NN_neural_network_t *nn = nullptr;
+  double learn = 0.003;
+
+  void run(int xDir, int yDir, float speed) {
+    runner = runner + speed * Vector2(xDir, yDir);
+  }
+
+  Game() {
+    nn = new NN_neural_network_t;
+
+    NN_info_t info;
+    info.hidden_layers_size = 1;
+    info.input_size = 4;
+    info.output_size = 2;
+    for (int i = 0; i < info.hidden_layers_size; i++)
+      info.neurons_per[i] = 6;
+    NN_init_neural_network(nn, &info);
+
+    for (int i = 0; i < 512; i++) {
+      Vector2 v;
+      v.x = NN_random(1, -0.5);
+      v.y = NN_random(1, -0.5);
+      trainingPts.push_back(v);
+      v.x = NN_random(1, -0.5);
+      v.y = NN_random(1, -0.5);
+      trainingPts2.push_back(v);
+    }
+  }
+
+  Vector2 targetSetup() {
+    Vector2 d;
+    if (runner.x < chaser.x)
+      d.x = -1.0;
+    if (runner.x > chaser.x)
+      d.x = +1.0;
+    if (runner.y < chaser.y)
+      d.y = -1.0;
+    if (runner.y > chaser.y)
+      d.y = +1.0;
+    return d;
+  }
+
+
+  void trainChaser() {
+    auto setup_target = [&]() {
+      Vector2 d;
+      Vector2 c(nn->input[0], nn->input[1]);
+      Vector2 p(nn->input[2], nn->input[3]);
+      if (p.x < c.x)
+        d.x = -1.0;
+      if (p.x > c.x)
+        d.x = +1.0;
+      if (p.y < c.y)
+        d.y = -1.0;
+      if (p.y > c.y)
+        d.y = +1.0;
+      return d;
+    };
+
+    size_t epochs = 0;
+    for (auto p : trainingPts) {
+      for (auto p2 : trainingPts2) {
+        nn->input[0] = p.x;
+        nn->input[1] = p.y;
+        nn->input[2] = p2.x;
+        nn->input[3] = p2.y;
+
+        auto d = setup_target();
+        nn->target[0] = d.x;
+        nn->target[1] = d.y;
+
+        NN_forward_propagate(nn);
+        NN_backward_propagate(nn, learn);
+        epochs++;
+      }
+    }
+    printf("# of epochs: %zu\n", epochs);
+  }
+
+  void chase() {
+    nn->input[0] = chaser.x;
+    nn->input[1] = chaser.y;
+    nn->input[2] = runner.x;
+    nn->input[3] = runner.y;
+
+    NN_forward_propagate(nn);
+
+    float x = nn->prediction[0];  // < -0.5f ? -1.0f : nn->prediction[0] > 0.5f ?  +1.0f : 0.0f;
+    float y = nn->prediction[1];  // < -0.5f ? -1.0f : nn->prediction[1] > 0.5f ?  +1.0f : 0.0f;
+
+    chaser = chaser + Vector2(x, y) * 0.005;
+
+//    printf("step %d\n", steps);
+//    printf(" * input...: %lf, %lf\n", nn->prediction[0], nn->prediction[1]);
+//    printf(" * position: %lf, %lf\n", chaser.x, chaser.y);
+  }
+
+  ~Game() {
+    delete nn;
+  }
+}
+;
---
+++ cpp/chase/main.cpp
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <functional>
+
+#include <mysdl2.h>	//https://github.com/frabbani/mysdl2
+
+#define DISP_W 512
+#define DISP_H 512
+
+#include "game.h"
+
+using namespace sdl2;
+
+SDL sdl;
+
+Game g;
+bool paused = true;
+
+void init() {
+	printf("*** INIT ***\n");
+	g.trainChaser();
+	g.runner = Vector2(0.0, 0.0);
+	g.chaser = Vector2(-0.45, -0.45);
+	paused = true;
+	printf("************\n");
+}
+
+void step() {
+	if( sdl.keyPress(SDLK_p))
+		paused = !paused;
+	if( paused )
+		return;
+
+	int x = 0, y = 0;
+	if (sdl.keyDown(SDLK_LEFT))
+		x--;
+	if (sdl.keyDown(SDLK_RIGHT))
+		x++;
+	if (sdl.keyDown(SDLK_UP))
+		y++;
+	if (sdl.keyDown(SDLK_DOWN))
+		y--;
+
+	g.run(x, y, 0.01);
+	g.chase();
+
+}
+
+
+
+void draw() {
+	auto pixels = sdl.lock();
+	auto draw_box = [&](Vector2 p, int w, int h, sdl2::Pixel24 color) {
+		h /= 2;
+		w /= 2;
+		for (int y = (int) p.y - h; y <= (int) p.y + h; y++)
+			for (int x = (int) p.x - w; x <= (int) p.x + w; x++)
+				pixels.plot(x, y, color);
+	};
+
+	pixels.inverted = true;
+	for (int y = 0; y < pixels.h; y++)
+		for (int x = 0; x < pixels.w; x++)
+			pixels.plot(x, y, 142, 195, 189);
+
+	Vector2 p;
+	Vector2 center = { DISP_W / 2, DISP_H / 2 };
+
+	/*
+	 for (auto v : g.trainingPts) {
+	 p.x = v.x * DISP_W;
+	 p.y = v.y * DISP_H;
+	 draw_box(center + p, 3, 3, Pixel24(250, 84, 84));
+	 }
+
+	 for (auto v : g.trainingPts2) {
+	 p.x = v.x * DISP_W;
+	 p.y = v.y * DISP_H;
+	 draw_box(center + p, 3, 3, Pixel24(84, 84, 250));
+	 }
+	 */
+
+	p.x = g.runner.x * DISP_W;
+	p.y = g.runner.y * DISP_H;
+	draw_box(center + p, 16, 16, Pixel24(123, 115, 170));
+
+	p.x = g.chaser.x * DISP_W;
+	p.y = g.chaser.y * DISP_H;
+	draw_box(center + p, 24, 25, Pixel24(183, 18, 98));
+}
+
+void term() {
+	printf("*** TERM ***\n");
+	printf("************\n");
+}
+
+int main(int argc, char *args[]) {
+	setbuf( stdout, NULL);
+
+	if (!sdl.init( DISP_W, DISP_H, false, "Run Boy Run!")) {
+		printf("SDL failed...?\n");
+		return 0;
+	}
+
+	double drawTimeInSecs = 0.0;
+	Uint32 drawCount = 0.0;
+	double invFreq = 1.0 / sdl.getPerfFreq();
+
+	auto start = sdl.getTicks();
+	auto last = start;
+	init();
+
+	while (true) {
+		auto now = sdl.getTicks();
+		auto elapsed = now - last;
+		if (sdl.keyDown(SDLK_ESCAPE))
+			break;
+		if (elapsed >= 20) {
+			sdl.pump();
+			step();
+			last = (now / 20) * 20;
+		}
+		Uint64 beginCount = sdl.getPerfCounter();
+		draw();
+		drawCount++;
+		sdl.swap();
+		drawTimeInSecs += (double) (sdl.getPerfCounter() - beginCount)
+				* invFreq;
+	}
+	term();
+	if (drawCount)
+		printf("Average draw time: %f ms\n",
+				(float) ((drawTimeInSecs * 1e3) / (double) drawCount));
+
+	sdl.term();
+
+	printf("goodbye!\n");
+	return 0;
+}
